2991e1b6c45bdc6cc7aad866af653718d2ddba7b
vijay <nimmagaddav9@gmail.com>
2020-11-13 23:06:59 -0800

    remove $

--- src/components/userTable/row.js
+++ src/components/userTable/row.js
@@ -4,12 +4,12 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 
 export const Row = (element, index) => (
   <tr>
-    <td>${index + 1}</td>
-    <td>${element.name}</td>
-    <td>${element.capital}</td>
-    <td>${element.region}</td>
-    <td>${element.population}</td>
-    <td>${element.nativeName}</td>
+    <td>{index + 1}</td>
+    <td>{element.name}</td>
+    <td>{element.capital}</td>
+    <td>{element.region}</td>
+    <td>{element.population}</td>
+    <td>{element.nativeName}</td>
     <td>
       <LazyLoadImage src={element.flag} width="100" height="50" effect="blur" />
     </td>
